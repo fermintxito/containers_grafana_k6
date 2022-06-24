a90ac728385228c6653e81abc103876e3ceccada
Fermin Gorraiz <fermin.gorraiz@gmail.com>
2022-06-24 13:08:21 +0200

    actualizar endpoint de prueba

--- scripts/ewoks.js
+++ scripts/ewoks.js
@@ -3,19 +3,19 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-      // Ramp-up from 1 to 5 virtual users (VUs) in 5s
-      { duration: "5s", target: 5 },
+      // Ramp-up from 1 to 50 virtual users (VUs) in 5s
+      { duration: "10s", target: 50 },
 
-      // Stay at rest on 5 VUs for 10s
-      { duration: "10s", target: 5 },
+      // Stay at rest on 50 VUs for 10s
+      { duration: "10s", target: 50 },
 
-      // Ramp-down from 5 to 0 VUs for 5s
+      // Ramp-down from 50 to 0 VUs for 5s
       { duration: "5s", target: 0 }
   ]
 };
 
 export default function () {
-  const response = http.get("https://swapi.dev/api/people/30/", {headers: {Accepts: "application/json"}});
+  const response = http.get("https://foxid5.einzelnet.com/api/public/health/test/messages/system", {headers: {Accepts: "application/json"}});
   check(response, { "status is 200": (r) => r.status === 200 });
   sleep(.300);
 };
